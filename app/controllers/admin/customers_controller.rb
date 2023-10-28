945b833351df8307b6e49103a046d7628ae6a666
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2023-10-28 06:24:54 +0000

    管理者側でゲストユーザーのアカウントが停止できなよう修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,10 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.page(params[:page], )
     @customer = current_customer
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
     @post_images = @customer.post_images.page(params[:page])
@@ -13,16 +13,19 @@ class Admin::CustomersController < ApplicationController
 
   def edit
   end
-  
+
+
   def out
-
     @customer = Customer.find(params[:id])
-    @customer.update(is_deleted: true) # または必要な更新処理を実行
-    # reset_session
-    redirect_to admin_post_images_path, notice: 'アカウントが停止しました'
 
+    if @customer.guest_customer?
+      redirect_to admin_customers_path, notice: 'ゲストユーザーは退会できません。'
+    else
+      @customer.update(is_deleted: true) # または必要な更新処理を実行
+      redirect_to admin_post_images_path, notice: 'アカウントが停止しました'
+    end
   end
-  
+
   private
 
   def customer_params
