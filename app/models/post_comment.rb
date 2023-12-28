9c2b6b347ea5b0b0243ed4621b1bef28a56fd6c8
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2023-12-28 13:35:19 +0000

    コメント追加

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,5 +2,6 @@ class PostComment < ApplicationRecord
     belongs_to :customer
     belongs_to :post_image
     
+    # 最大250文字まで
     validates :comment, presence: true, length: { maximum: 250 }
 end
