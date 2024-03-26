9ce52e90cd75493a38fd0f140f15ba69def2fd56
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2024-03-26 04:45:13 +0000

    コメントの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
    before_action :authenticate_admin!, if: :admin_url
    
    
+   #pathに/adminが含まれている全てのページは、adminでログインしないと見ることができない
   def admin_url
     request.fullpath.include?("/admin")
   end
